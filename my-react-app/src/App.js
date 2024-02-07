f8529140f0ce4279cc7e661e40dc8b2182f65223
DeepikaSR4 <deepikasanathanan1234@gmail.com>
2024-02-07 11:22:27 +0530

    Initial commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Navbar} from "./components/Navbar";
+import {Products} from "./components/Products"
+function App() {
+  return (
+    <>
+     <Navbar />
+    <Products />
+    </>
+    );
+}
+export default App;
